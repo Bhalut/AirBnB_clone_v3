0ecf4ae61d28a6ebd7994dcdff43b7fcb20de157
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-01-27 15:43:38 -0500

    5th task

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
+from api.v1.views import app_views
+from flask import Flask, jsonify, make_response, render_template, url_for
+from models import storage
+import os
+
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """method to handle @app.teardown_appcontext
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    """MAIN"""
+    app.run(host=host, port=port, threaded=True)
