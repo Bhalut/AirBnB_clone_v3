d6516b5a71226b9cb2805a21a9c923a87272395c
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-01-27 17:08:24 -0500

    5th task

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,15 +5,19 @@ Flask App that integrates with AirBnB static HTML Template
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response, render_template, url_for
 from models import storage
+from flasgger import Swagger
 import os
 
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 app.url_map.strict_slashes = False
 
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
+
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
