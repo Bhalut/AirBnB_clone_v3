d5b3aec2c121e07bd5415ce40a01125c62a80e2d
Abdel Mejia <bhalut.bear@gmail.com>
2021-01-28 05:10:59 +0000

    Add api/v1 cities.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,9 @@
-from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views.states import *
+#!/usr/bin/python3
+"""Init module
+"""
+from api.v1.views.cities import *
 from api.v1.views.index import *
+from api.v1.views.states import *
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
