4505ce153256511ee32be8b02dd0cdaed6f954ef
Abdel Mejia <bhalut.bear@gmail.com>
2021-01-29 05:02:42 +0000

    add app file at api/v1

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,42 +1,40 @@
 #!/usr/bin/python3
-"""
-Flask REST API module
-"""
-from api.v1.views import app_views
-from flask import Flask, jsonify, make_response, render_template, url_for
-from models import storage
-from flasgger import Swagger
-from flask_cors import CORS
-import os
+""" app
 
+    Flask REST API module
+"""
+from flask import Flask, Blueprint, make_response, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
-swagger = Swagger(app)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-app.url_map.strict_slashes = False
-
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = os.getenv('HBNB_API_PORT', 5000)
-
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """method to handle @app.teardown_appcontext
-    """
+def close(error):
+    """ Call method close() from storage """
     storage.close()
 
 
 @app.errorhandler(404)
-def handle_404(exception):
-    """handles 404 errors"""
-    code = exception.__str__().split()[0]
-    message = {'error': "Not found"}
-    return make_response(jsonify(message), code)
+def not_found(error):
+    """Response for error code 404
+
+    Args:
+        error
+
+    Returns:
+        response
+    """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    """MAIN"""
-    app.run(host=host, port=port, threaded=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host, port, threaded=True)
