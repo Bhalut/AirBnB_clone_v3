e64bda858cce11627516c3013a2eccad407b9fc5
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-02-25 22:39:15 -0500

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Flask route that returns json status response
+"""
+Flask route that returns json status response
 """
 from api.v1.views import app_views
 from flask import jsonify, request
@@ -8,7 +9,9 @@ from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    """returns the status"""
+    """
+    function for status route that returns the status
+    """
     if request.method == 'GET':
         resp = {"status": "OK"}
         return jsonify(resp)
@@ -16,7 +19,9 @@ def status():
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
-    """return the count of all class objects"""
+    """
+    function to return the count of all class objects
+    """
     if request.method == 'GET':
         response = {}
         PLURALS = {
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Flask route that returns json status response"""
+"""
+Flask route that returns json status response
+"""
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from flasgger import Swagger, swag_from
@@ -9,7 +11,9 @@ from models import storage, CNC
 @app_views.route('/states', methods=['GET', 'POST'])
 @swag_from('swagger_yaml/states_no_id.yml', methods=['GET', 'POST'])
 def states_no_id():
-    """Method to handle http for requested states."""
+    """
+        states route to handle http method for requested states no id provided
+    """
     if request.method == 'GET':
         all_states = storage.all('State')
         all_states = list(obj.to_json() for obj in all_states.values())
@@ -30,7 +34,9 @@ def states_no_id():
 @app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
 @swag_from('swagger_yaml/states_id.yml', methods=['PUT', 'GET', 'DELETE'])
 def states_with_id(state_id=None):
-    """Method to handle http for requested state by id."""
+    """
+        states route to handle http method for requested state by id
+    """
     state_obj = storage.get('State', state_id)
     if state_obj is None:
         abort(404, 'Not found')
